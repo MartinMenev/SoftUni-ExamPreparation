6fb89521e248a7024ff0bc081b75652796db58c3
MartinMenev <martin.menew@gmail.com>
2022-10-12 08:09:33 +0300

    New code

---
+++ src/bakery/Employee.java
@@ -0,0 +1,31 @@
+package bakery;
+
+public class Employee {
+    private final String name;
+    private final int age;
+    private final String country;
+
+    public Employee(String name, int age, String country) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("Employee: %s, %d (%s)", name, age, country);
+    }
+}
